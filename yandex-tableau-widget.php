c6667cb4f0e8ffec85184f9ab3132f5219651f8c
dikiyforester <frolov.en@gmail.com>
2015-10-09 12:56:39 +0500

    Initial commit

---
+++ yandex-tableau-widget.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Yandex Tableau Widget
+Plugin URI:  https://wordpress.org/plugins/yandex-tableau-widget/
+Description: The Yandex Tableau Widget plugin allows to customize your WordPress site representation on the Yandex.Browser Tableau.
+Version:     1.0
+Author:      Artem Frolov
+Author URI:  https://profiles.wordpress.org/dikiy_forester
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: afaytw
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
